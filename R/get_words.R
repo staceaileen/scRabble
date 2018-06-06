0cfa1fedbdc3f5ea54c62fd8c7b61c18ac591b9f
Stacy K <stacykam@mail.usf.edu>
2018-06-06 14:25:50 +0200

    Added functionality to get_words(), halfway through

--- R/get_words.R
+++ R/get_words.R
@@ -9,9 +9,22 @@
 #' @examples
 #' get_words("uhwoih")
 #' get_words("ihfjwd", 4)
+#' @import stringr str_c
+#' @import rvest
+#' @import dplyr
+#' @import xml2
 
-get_words <- function(letters, word_len = 0){
+get_words <- function(letters, word_len = 0, wildcard = FALSE){
 
-
-
+  link = stringr::str_c("https://word.tips/words-for/", letters, "?is_search=true")
+  html <- xml2::read_html(link)
+  html <- rvest::html_nodes(html, xpath = '//*[contains(concat( " ", @class, " " ), concat( " ", "changeable-word", " " ))]')
+  html <- rvest::html_text(html)
+  if(word_len == 0) {html} else{
+    for(i in html){
+      if (length(i) == word_len){
+        # something here....
+      }
+    }
+  }
 }
