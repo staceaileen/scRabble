cb8ef79ba2e2b4ab31fbb006011cc63e1e071e33
Stacy K <stacykam@mail.usf.edu>
2018-06-06 13:27:37 +0200

    Filled in roxygen

---
+++ R/get_words.R
@@ -0,0 +1,17 @@
+#' Get words from random letters
+#'
+#' @param letters random string of letters
+#' @param word_len length of desired word
+#'
+#' @return returns a list with words made up from string of letters
+#' @export
+#'
+#' @examples
+#' get_words("uhwoih")
+#' get_words("ihfjwd", 4)
+
+get_words <- function(letters, word_len = 0){
+
+
+
+}
