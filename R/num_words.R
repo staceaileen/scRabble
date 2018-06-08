b2ec022cac80e2df8d946a471cadcc2160712199
Stacy K <stacykam@mail.usf.edu>
2018-06-08 12:31:33 +0200

    Created new function num_words and it works!

---
+++ R/num_words.R
@@ -0,0 +1,19 @@
+#' Number of words
+#'
+#' @param letters string of characters to build words
+#' @param word_len if desired, length of words to be returned
+#' @param wildcard boolean to activate or not a wildcard
+#'
+#' @return returns an int with the number of possible words you can create
+#' @export
+#'
+#' @examples
+#' get_words("uhwoih")
+#' get_words("ihfjwd", word_len = 4)
+#' get_words("hasfic", wildcard = TRUE)
+
+num_words <- function(letters, word_len = 0, wildcard = FALSE){
+  words <- get_words(letters, word_len, wildcard)
+  num <- length(words)
+  return(num)
+}
