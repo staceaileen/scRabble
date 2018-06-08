73eafe0d4f424814ced39f35581f17705e7e45c1
Aaron Balleisen <aaronsb@princeton.edu>
2018-06-08 18:07:06 +0200

    initial commit

---
+++ R/tile_values.R
@@ -0,0 +1,34 @@
+if (FALSE) {
+   tile_values <- read.table(text = "
+    letter  value
+  1 a 1
+  2 b 3
+  3 c 3
+  4 d 2
+  5 e 1
+  6 f 4
+  7 g 2
+  8 h 4
+  9 i 1
+  10  j 8
+  11  k 5
+  12  l 1
+  13  m 3
+  14  n 1
+  15  o 1
+  16  p 3
+  17  q 10
+  18  r 1
+  19  s 1
+  20  t 1
+  21  u 1
+  22  v 4
+  23  w 4
+  24  x 8
+  25  y 4
+  26  z 10
+  27  ? 0
+                            ")
+}
+
+devtools:: use_data(tile_values, overwrite = TRUE)
