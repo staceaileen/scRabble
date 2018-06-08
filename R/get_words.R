1beb7c3c17b54656da2e9a1952608a43a246098a
Stacy K <stacykam@mail.usf.edu>
2018-06-08 12:02:50 +0200

    Got the get_words() function working! Woooo

--- R/get_words.R
+++ R/get_words.R
@@ -2,13 +2,15 @@
 #'
 #' @param letters random string of letters
 #' @param word_len length of desired word
+#' @param wildcard boolean indicating if there's a wildcard
 #'
 #' @return returns a list with words made up from string of letters
 #' @export
 #'
 #' @examples
 #' get_words("uhwoih")
-#' get_words("ihfjwd", 4)
+#' get_words("ihfjwd", word_len = 4)
+#' get_words("hasfic", wildcard = TRUE)
 #' @import stringr str_c
 #' @import rvest
 #' @import dplyr
@@ -16,15 +18,18 @@
 
 get_words <- function(letters, word_len = 0, wildcard = FALSE){
 
-  link = stringr::str_c("https://word.tips/words-for/", letters, "?is_search=true")
+  if(wildcard == TRUE){
+    link = link = stringr::str_c("https://word.tips/words-for/", letters, "*?is_search=true")
+  } else {
+    link = stringr::str_c("https://word.tips/words-for/", letters, "?is_search=true")
+  }
+
   html <- xml2::read_html(link)
   html <- rvest::html_nodes(html, xpath = '//*[contains(concat( " ", @class, " " ), concat( " ", "changeable-word", " " ))]')
   html <- rvest::html_text(html)
-  if(word_len == 0) {html} else{
-    for(i in html){
-      if (length(i) == word_len){
-        # something here....
-      }
-    }
+  if(word_len == 0) {return(html)} else{
+    condition <- sapply(html, function(x) nchar(x) == word_len)
+    return (html[condition])
   }
 }
+
